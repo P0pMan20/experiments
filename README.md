f9d20cd39ed6295ffa2737b75028b7363180712c
P0pMan20 <51844807+P0pMan20@users.noreply.github.com>
2023-02-05 22:15:54 +1000

    a tired, worked on this for a little bit and it ended up putting me to sleep

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ My JavaScript Experiments
 
 Currently contains
 - [Slider](https://p0pman20.github.io/experiments/img_slider/) - image comparision tool?
+- [Canvas Trees](https://p0pman20.github.io/experiments/canvas_trees/) - WIP trees drawn via html canvas
--- canvas_trees/trees.js
+++ canvas_trees/trees.js
@@ -1,6 +1,6 @@
-let treeBranchyness = 0.4;
-let treeHeightBeforeBranch = 0.6;
-let treeHeight = 200; //200 px
+// let treeBranchyness = 0.4;
+// let treeHeightBeforeBranch = 0.6;
+// let treeHeight = 200; //200 px
 let treeLeafColour = "#00cc00";
 let treeWoodColour = "#663300";
 
@@ -12,9 +12,47 @@ function init(){
     let treeWoodColourPicker = document.querySelector("#treeWoodColour");
     treeWoodColourPicker.addEventListener("input", () => {
         treeWoodColour = treeWoodColourPicker.value;
+        console.log(treeWoodColour)
     })
 
 
+}
+
+function drawRectRotatedAroundCentre(ctx ,x, y, w, h, radians, fillstyle){
+    //ty https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Transformations
+    /*
+    To rotate the rectangle around its own center, we translate the canvas to the center of the rectangle,
+    then rotate the canvas, then translate the canvas back to 0,0, and then draw the rectangle.
+    */
+
+    console.log(x, y, w, h, radians, fillstyle)
+    const halfRectX = x+0.5*w;
+    const halfRectY = y+0.5*h;
+
+    ctx.save();
+    ctx.translate(halfRectX, halfRectY);
+    ctx.rotate(radians);
+    ctx.translate(-halfRectX, -halfRectY);
+    ctx.fillStyle = fillstyle;
+    ctx.fillRect(x, y, w, h);
+    ctx.restore()
+
+}
+
+
+class Tree {
+    baseX;
+    baseY;
+    baseTrunkWidth;
+    treeHeight;
+    treeHeightBeforeBranch;
+    constructor(baseX, baseY, baseTrunkWidth, treeHeight, treeHeightBeforeBranch) {
+        this.baseX = baseX;
+        this.baseY = baseY;
+        this.baseTrunkWidth = baseTrunkWidth;
+        this.treeHeight = treeHeight;
+        this.treeHeightBeforeBranch = treeHeightBeforeBranch;
+    }
 }
 
 
@@ -22,5 +60,12 @@ function init(){
 function generateTree(){
     let canvas = document.querySelector("#treeCanvas");
     let canvasCtx = canvas.getContext("2d");
-    
+    let tree = new Tree(Math.round(canvas.width/2 - 30/2), Math.round(canvas.height * 0.9)-250, 30, 250, 1)
+
+
+    drawRectRotatedAroundCentre(canvasCtx, tree.baseX, tree.baseY, tree.baseTrunkWidth, tree.treeHeight, 180*(Math.PI/180), treeWoodColour)
+    drawRectRotatedAroundCentre(canvasCtx, tree.baseX+tree.treeHeight * 0.3-30, tree.baseY+(tree.treeHeightBeforeBranch*-tree.treeHeight)+tree.treeHeight,  tree.baseTrunkWidth * 0.8, tree.treeHeight * 0.3, Math.PI*(3/2), treeWoodColour  )
+
+
+
 }
